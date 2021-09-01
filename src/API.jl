58b0f57be2d9f0c22a7bbecb631756c623414b94
Philip Bittihn <philip@bittihn.de>
2021-09-01 23:34:44 +0200

    let get_state default to current state

--- src/API.jl
+++ src/API.jl
@@ -4,6 +4,7 @@ export get_state, get_state_on_load
 get_state_on_load(m::Module) = get_state_on_load(m::Module, nothing)
 get_state_on_load(m::Module, prop::Union{Nothing,Symbol}) = get_state(m, prop, 1)
 
+get_state(m::Module) = current_package_state(m)
 get_state(m::Module, idx) = get_state(m, nothing, idx)
 function get_state(m::Module, prop::Union{Nothing,Symbol}, idx)
     prop !== nothing && !hasfield(PackageState, prop) && error("Can only get properties ", fieldnames(PackageState))
